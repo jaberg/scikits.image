febf961c053cee8bf9fe6924b649fabd7995b0f2
James Bergstra <james.bergstra@gmail.com>
2010-07-13 23:01:35 -0400

    added first unit test for givens argument

--- scikits/lazy/tests/test_lazy.py
+++ scikits/lazy/tests/test_lazy.py
@@ -1,5 +1,14 @@
 import unittest
-from scikits.lazy import FunctionClosure
+from scikits.lazy import function, Symbol, Impl
+
+@Impl.allow_lazy()
+def dummy_fn(a):
+    return a + 5
+
+def func(*args, **kwargs):
+    return function(
+            transform_policy_ctor=lambda :(lambda x:x),
+            *args,**kwargs) 
 
 class Test_FunctionClosure_new_givens(unittest.TestCase):
     """Test the givens argument to FunctionClosure.new """
@@ -9,6 +18,18 @@ class Test_FunctionClosure_new_givens(unittest.TestCase):
     # test that you can create an unattached symbol as a value, give it attributes
     # like a shape, and an allocation_target, and a name, and a 'strict=True' and those values
     # will be respected by the FunctionClosure
+    def test_behaviour_0(self):
+        s = Symbol.new(name='s')
+
+        f = func([s], dummy_fn(s))
+        assert f[s].name=='s'
+
+        f = func([s], dummy_fn(s), givens={s:Symbol.new_kwargs(
+            name='u', strict=True, shape=(4,5,6), opencl_ctx='blah')})
+        assert f[s].name == 'u'
+        assert f[s].strict==True
+        assert f[s].shape == (4,5,6)
+        assert f[s].opencl_ctx=='blah'
 
 
     # Unit tests
